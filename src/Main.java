2809fe51ba98369de222724b5563114fdc7969fd
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-08-26 09:26:31 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+        System.out.println(isCatPlaying(true,45));
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        if(summer){
+            return temperature >= 25 && temperature <= 45;
+        }else{
+            return temperature >= 25 && temperature <= 35;
+        }
     }
 }
